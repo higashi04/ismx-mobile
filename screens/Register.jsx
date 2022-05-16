b060a21a7f542c5f1291a405cf80d02550799ff2
Mac <mac@Mac-mini-de-Mac.local>
2022-05-15 21:14:06 -0500

    finish register screen, moved svgs to their own file in assets folder

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -11,29 +11,14 @@ import { SvgXml } from "react-native-svg";
 import  DateTimePicker  from "@react-native-community/datetimepicker";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
-const hammerSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hammer" viewBox="0 0 16 16">
-<path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5.009 5.009 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334z"/>
-</svg>`
-const personCircleSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-</svg>`
-const EyeFill = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-<path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-<path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-</svg>`;
-const EyeSlashed = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
-<path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
-<path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
-</svg>`;
-const calendarSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
-<path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
-<path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-</svg>`
+import { emailSvg, personCircleSvg, EyeFill, EyeSlashed, calendarSvg, phoneSvg } from "../assets/svgsPaths";
+
+
 
 const Register = () => {
     const [text, SetText] = useState("");
     const [toggle, setToggle] = useState(true)
+    const [toggleConfirm, setToggleConfirm] = useState(true)
     const [date, setDate] = useState(new Date());
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
@@ -56,11 +41,14 @@ const Register = () => {
     function changeToggle() {
         setToggle(prevToggle => !prevToggle)
     }
+    function changeToggleConfirm() {
+        setToggleConfirm(prevToggle => !prevToggle)
+    }
 
     return(
         <ScrollView style={styles.body}>
             <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ?  'padidng' : 'height'}
+                behavior={Platform.OS === 'ios' ?  'padding' : 'height'}
             >
             <View style={styles.registerBox}>
                 <View style={styles.box}>
@@ -76,9 +64,50 @@ const Register = () => {
                 <Text style={styles.inputLabel}>Usuario</Text>
                 </View>
                 <TextInput 
-                    style={styles.input} 
+                    style={styles.input}
+                    label='username' 
                     placeholder='Escriba el nombre de su cuenta'
                     placeholderTextColor='#eab948'
+                 />
+                 <View style={styles.box}>
+                    <SvgXml
+                        style={styles.svg}
+                        xml={emailSvg}
+                        width="32"
+                        height="32"
+                        stroke="black"
+                        strokeWidth=".5"
+                        fill="black"
+                        viewBox="0 0 32 32"
+                    />
+                    <Text style={styles.inputLabel}>Email</Text>
+                 </View>
+                 <TextInput
+                    style={styles.input}
+                    label='email'
+                    keyboardType="email-address"
+                    placeholder="ejemplo@mail.com"
+                    placeholderTextColor='#eab948' 
+                 />
+                 <View style={styles.box}>
+                     <SvgXml
+                        style={styles.svg}
+                        xml={phoneSvg}
+                        width="32"
+                        height="32"
+                        stroke="black"
+                        strokeWidth=".5"
+                        fill="black"
+                        viewBox="0 0 32 32"
+                     />
+                     <Text style={styles.inputLabel}>Teléfono</Text>
+                 </View>
+                 <TextInput
+                    style={styles.input}
+                    label='phone'
+                    keyboardType="phone-pad"
+                    placeholder="123 456 78 91"
+                    placeholderTextColor='#eab948' 
                  />
                  <View style={styles.box}>
                 <SvgXml
@@ -93,15 +122,37 @@ const Register = () => {
                     onPress={changeToggle}
                 />
                 <Text style={styles.inputLabelPass}>Contraseña</Text>
-            </View>
-            <TextInput
-                onChangeText={SetText}
-                secureTextEntry={toggle ? true : false}
-                style={styles.input}
-                value={text}
-                placeholder={"Contraseña"}
-                placeholderTextColor='#eab948'
-            />
+                </View>
+                <TextInput
+                    onChangeText={SetText}
+                    secureTextEntry={toggle ? true : false}
+                    style={styles.input}
+                    value={text}
+                    label='password'
+                    placeholder={"Contraseña"}
+                    placeholderTextColor='#eab948'
+                />
+                <View style={styles.box}>
+                <SvgXml
+                    xml={toggleConfirm ? EyeFill : EyeSlashed}
+                    style={styles.svgEye}
+                    width="48"
+                    height="48"
+                    stroke="black"
+                    strokeWidth=".5"
+                    fill="black"
+                    viewBox="0 0 32 32"
+                    onPress={changeToggleConfirm}
+                />
+                    <Text style={styles.inputLabelPass}>Confirmar Contraseña</Text>
+                </View>
+                <TextInput
+                    secureTextEntry={toggleConfirm ? true : false}
+                    style={styles.input}
+                    label='passwordConfirm'
+                    placeholder={"Confirmar Contraseña"}
+                    placeholderTextColor='#eab948'
+                />
                 <View style={styles.box}>
                     <SvgXml
                         xml={calendarSvg}
@@ -114,21 +165,27 @@ const Register = () => {
                     />
                     <Text style={styles.inputLabelPass}>Fecha de Nacimiento </Text>
                 </View>
-                <Pressable style={styles.submit} onPress={showDatepicker} >
-                    <Text style={styles.btnText}>
-                        Presione para mostrar el calendario
-                    </Text>
-                </Pressable>
-                <Text> Fecha Seleccionada: {date.toLocaleDateString()} </Text>
+                <View style={styles.box}>
+                    <Text style={styles.inputLabel}> {date.toLocaleDateString()} </Text>
+                </View>
+                <View style={styles.datePicker}>
                 {show && (
                     <DateTimePicker
                     testID='dateTimePicker'
                     value={date}
                     mode={mode}
                     is24Hour={true}
+                    label='dob'
                     onChange={onChange}
                     />
                 )}
+                </View>
+                <Pressable style={styles.submit} onPress={showDatepicker} >
+                    <Text style={styles.btnText}>
+                        Calendario
+                    </Text>
+                </Pressable>
+
             </View>
             
 
@@ -139,13 +196,14 @@ const Register = () => {
 
 const styles = StyleSheet.create({
     body: {
-        backgroundColor: '#eab948'
+        backgroundColor: '#eab948',
+        height: 990
     },
     registerBox: {
         backgroundColor: "#fff",
         borderRadius: 20,
         marginTop: 50,
-        height: 600,
+        height: 900,
         marginHorizontal: 10
     },
     input: {
@@ -193,6 +251,11 @@ const styles = StyleSheet.create({
       btnText: {
         fontSize: 15,
       },
+      datePicker: {
+          justifyContent: 'center',
+          paddingRight: 120,
+          paddingTop: 20
+      }
 })
 
 export default Register;
